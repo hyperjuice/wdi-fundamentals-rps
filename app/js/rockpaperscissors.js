1025bd51b630813a96ae26ceddbb7e158244f831
Darrell Cohn <darrellcohn@gmail.com>
2015-03-05 14:04:47 -0800

    added code for function getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,36 +25,30 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
-	
-// start my work
-
-if (move >= 1 || move <= 1) {
-    console.log(move);
-} else {
-    console.log(getInput());
-}
-
-// end my work
-
+    if (move === "rock") {
+        return [move];
+    } else if (move === "paper") {
+        return [move];
+    } else if (move === "scissors") {
+        return [move];
+    } else {
+        return [getInput]
+    }
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
-
-// start my work
-
-if (move >= 1 || move <= 1) {
-    console.log(move);
-} else {
-    console.log(randomPlay());
-}
-
-// end my work
-
+    if (move === "rock") {
+        return [move];
+    } else if (move === "paper") {
+        return [move];
+    } else if (move === "scissors") {
+        return [move];
+    } else {
+        return [randomPlay]
+    }
 }
 
 function getWinner(playerMove,computerMove) {
@@ -63,6 +57,25 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === "rock" && computerMove === "scissors") {
+        winner = player;
+    } else if (playerMove === "scissors" && computerMove === "paper") {
+        winner = player;
+    } else if (playerMove === "paper" && computerMove === "rock") {
+        winner = player;
+    } else if (computerMove === "rock" && playerMove === "scissors") {
+        winner = computer;
+    } else if (computerMove === "scissors" && playerMove === "paper") {
+        winner = computer;
+    } else if (computerMove === "paper" && playerMove === "rock") {
+        winner = computer;
+    } else if (computerMove === "rock" && playerMove === "rock") {
+        winner = tie;
+    } else if (computerMove === "scissors" && playerMove === "scissors") {
+        winner = tie;
+    } else {
+        winner = tie;
+    }
     return winner;
 }
 
